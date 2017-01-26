7798645709d8acb7f972329816fec2660d939b8c
robin.ustarroz@epitech.eu <robin.ustarroz@epitech.eu>
2017-01-26 15:31:13 +0100

    malloc.c

---
+++ malloc.c
@@ -0,0 +1,28 @@
+/*
+** malloc.c for bonjour in /home/voyevoda/rendu/systeme_unix/PSU_2016_malloc
+**
+** Made by voyevoda
+** Login   <voyevoda@epitech.net>
+**
+** Started on  Thu Jan 26 14:51:23 2017 voyevoda
+** Last update Thu Jan 26 15:30:02 2017 voyevoda
+*/
+
+#include <unistd.h>
+#include "malloc.h"
+
+
+void	*malloc(size_t size)
+{
+
+}
+
+void	free(void *ptr)
+{
+
+}
+
+void	*realloc(void *ptr, size_t size)
+{
+
+}
